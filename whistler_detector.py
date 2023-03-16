371a7d315807b4d32a22ff886b271207e3f2d104
Zach Lee <zlee@vt.edu>
2023-03-15 23:44:18 -0400

    whistler detect on random values

--- whistler_detector.py
+++ whistler_detector.py
@@ -1,49 +1,43 @@
-
-import tensorflow_io as tfio
+# import tensorflow_io as tfio
 import tensorflow as tf
-from tensorflow import keras
-#from tensorflow.keras import layers
-#import matplotlib.pyplot as plt
-#import pathlib
-import os
+# from tensorflow import keras
+# #from tensorflow.keras import layers
+# #import matplotlib.pyplot as plt
+# #import pathlib
+# import os
 import numpy as np
-from scipy import signal
-from scipy.io.wavfile import read
-#import random
+# from scipy import signal
+# from scipy.io.wavfile import read
+# #import random
 
-from PIL import Image
+# from PIL import Image
 
+# TF_CPP_MIN_LOG_LEVEL="2"
 
-TF_CPP_MIN_LOG_LEVEL="2"
-
-
-
-
-
-def wav2set(filename):
-    file = tf.io.read_file(filename)
-    signal, sample_rate = tf.audio.decode_wav(file, desired_channels = 1)
-    return signal, sample_rate
+# def wav2set(filename):
+#     file = tf.io.read_file(filename)
+#     signal, sample_rate = tf.audio.decode_wav(file, desired_channels = 1)
+#     return signal, sample_rate
     
-def preprocess(filename, label):
-        #READING SIGNAL FROM WAV FILES
-    wav, rate = wav2set(filename)
+# def preprocess(filename, label):
+#         #READING SIGNAL FROM WAV FILES
+#     wav, rate = wav2set(filename)
     
-        #Shift randomly, 2.5 sec to the right or left
-    # splitpoint = random.randint(0, 159822) - 79911
-    # if splitpoint < 0:
-    #     splitpoint = 191786 + splitpoint
-    # firstwav = wav[:splitpoint]
-    # secondwav = wav[splitpoint:]
-    # wav = tf.concat([secondwav, firstwav], axis=0)
+#         #Shift randomly, 2.5 sec to the right or left
+#     # splitpoint = random.randint(0, 159822) - 79911
+#     # if splitpoint < 0:
+#     #     splitpoint = 191786 + splitpoint
+#     # firstwav = wav[:splitpoint]
+#     # secondwav = wav[splitpoint:]
+#     # wav = tf.concat([secondwav, firstwav], axis=0)
     
-    spectrogram = tf.abs(tf.signal.stft(tf.transpose(wav[:191786]), frame_length=256, frame_step=256))
-    #spectrogram = tf.expand_dims(spectrogram, axis=2)
-    spectrogram = tf.squeeze(tf.transpose(spectrogram))
+#     spectrogram = tf.abs(tf.signal.stft(tf.transpose(wav[:191786]), frame_length=256, frame_step=256))
+#     #spectrogram = tf.expand_dims(spectrogram, axis=2)
+#     spectrogram = tf.squeeze(tf.transpose(spectrogram))
     
-    #try adding log
-    #print("spectrogram\n", spectrogram)
-    return spectrogram, label
+#     #try adding log
+#     #print("spectrogram\n", spectrogram)
+#     return spectrogram, label
 
 
 # Load the TFLite model and allocate tensors.
@@ -65,3 +59,4 @@ interpreter.invoke()
 # Use `tensor()` in order to get a pointer to the tensor.
 output_data = interpreter.get_tensor(output_details[0]['index'])
 print(output_data)
+#quit()

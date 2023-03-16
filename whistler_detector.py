aeaef4de584635d4a376d84d8040574bb46975b4
Zach Lee <zlee@vt.edu>
2023-03-16 00:19:24 -0400

    changed whistler for board

--- whistler_detector.py
+++ whistler_detector.py
@@ -1,5 +1,5 @@
 # import tensorflow_io as tfio
-import tensorflow as tf
+#import tensorflow as tf
 # from tensorflow import keras
 # #from tensorflow.keras import layers
 # #import matplotlib.pyplot as plt
@@ -9,6 +9,7 @@ import numpy as np
 # from scipy import signal
 # from scipy.io.wavfile import read
 # #import random
+import tflite_runtime.interpreter as tflite
 
 # from PIL import Image
 
@@ -41,7 +42,7 @@ import numpy as np
 
 
 # Load the TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter = tflite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
 
 # Get input and output tensors.
@@ -55,8 +56,51 @@ interpreter.set_tensor(input_details[0]['index'], input_data)
 
 interpreter.invoke()
 
-# The function `get_tensor()` returns a copy of the tensor data.
-# Use `tensor()` in order to get a pointer to the tensor.
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print(output_data)
-#quit()
+# # The function `get_tensor()` returns a copy of the tensor data.
+# # Use `tensor()` in order to get a pointer to the tensor.
+# output_data = interpreter.get_tensor(output_details[0]['index'])
+# print(output_data)
+# #quit()
+
+# class TestModel(tf.Module):
+#   def __init__(self):
+#     super(TestModel, self).__init__()
+
+#   @tf.function(input_signature=[tf.TensorSpec(shape=[1, 10], dtype=tf.float32)])
+#   def add(self, x):
+#     '''
+#     Simple method that accepts single input 'x' and returns 'x' + 4.
+#     '''
+#     # Name the output 'result' for convenience.
+#     return {'result' : x + 4}
+
+
+# SAVED_MODEL_PATH = 'content/saved_models/test_variable'
+# TFLITE_FILE_PATH = 'model.tflite'
+
+# # Save the model
+# module = TestModel()
+# # You can omit the signatures argument and a default signature name will be
+# # created with name 'serving_default'.
+# tf.saved_model.save(
+#     module, SAVED_MODEL_PATH,
+#     signatures={'my_signature':module.add.get_concrete_function()})
+
+# # Convert the model using TFLiteConverter
+# converter = tf.lite.TFLiteConverter.from_saved_model(SAVED_MODEL_PATH)
+# tflite_model = converter.convert()
+# with open(TFLITE_FILE_PATH, 'wb') as f:
+#   f.write(tflite_model)
+
+# # Load the TFLite model in TFLite Interpreter
+# interpreter = tf.lite.Interpreter(TFLITE_FILE_PATH)
+# # There is only 1 signature defined in the model,
+# # so it will return it by default.
+# # If there are multiple signatures then we can pass the name.
+# my_signature = interpreter.get_signature_runner()
+
+# # my_signature is callable with input as arguments.
+# output = my_signature(x=tf.constant([1.0], shape=(1,10), dtype=tf.float32))
+# # 'output' is dictionary with all outputs from the inference.
+# # In this case we have single output 'result'.
+# print(output['result'])
